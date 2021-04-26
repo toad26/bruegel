c59366ef02c5ac6402c3e7f31847ee02766e2628
Marija <marija_toda@yahoo.com>
2021-04-26 11:29:50 +0200

    initial

---
+++ src/main/java/net/bruegel/BruegelApplication.java
@@ -0,0 +1,13 @@
+package net.bruegel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruegelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BruegelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/bruegel/BruegelApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bruegel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruegelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
